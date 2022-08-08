7c19ffd9b19662dc96f11b85b2c17fb6b7d3c3e9
renatotnk <renato-tnk@live.com>
2022-08-08 15:40:34 +0100

    First Component...

---
+++ exercicios/pages/basicos/usandoTitulos.jsx
@@ -0,0 +1,12 @@
+import Titulo from "../../components/Titulo"
+
+export default function usandoTitulo() {
+    return(
+        <div>
+            <h1>Usando Título</h1>
+            <Titulo/>
+            <Titulo/>
+            <Titulo/>
+        </div>
+    )
+}
