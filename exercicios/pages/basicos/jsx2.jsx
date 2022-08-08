ab960258e0afc498e00e6cfcc598877b94fc67e5
renatotnk <renato-tnk@live.com>
2022-08-08 14:21:44 +0100

    Integrating JSX and JS...

--- exercicios/pages/basicos/jsx2.jsx
+++ exercicios/pages/basicos/jsx2.jsx
@@ -1,7 +1,7 @@
 export default function jsx3(){
     return (
         <div>
-            <h1></h1>
+            <h1>ALOU</h1>
             <div id="legal"></div>
             <span className="opa"></span>
         </div>
