cd23e46964cc516efeb7b34d561160f18bc7ce3b
renatotnk <renato-tnk@live.com>
2022-08-08 14:01:57 +0100

    Second commit, with some changes in the basic examples...

---
+++ exercicios/pages/basicos/jsx1.js
@@ -0,0 +1,13 @@
+export default function jsx1() {
+    const array = [
+        <li>Paulo</li>,
+        <li>Pedro</li>,
+        <li>Ana</li>
+    ]
+    const conteudo = (
+        <div>
+            <h1>JSX #01</h1>
+        </div>
+    );
+    return conteudo;
+}
