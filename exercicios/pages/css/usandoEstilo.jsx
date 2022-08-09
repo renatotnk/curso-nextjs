bc129391708a87c8baeaed2e20e7662cbd34acbb
renatotnk <renato-tnk@live.com>
2022-08-09 13:09:18 +0100

    Working with conditional styling...

---
+++ exercicios/pages/css/usandoEstilo.jsx
@@ -0,0 +1,10 @@
+import Estilo from "../../components/Estilo";
+
+export default function usandoEstilo() {
+    return (
+        <>
+            <Estilo numero={3} color="#0ff"/>
+            <Estilo numero={-13} color="#fff" direita/>
+        </>
+    )
+}
