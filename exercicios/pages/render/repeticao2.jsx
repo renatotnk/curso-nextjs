2905e6093db5ee5430a8333fe4845adcf060e9ea
renatotnk <renato-tnk@live.com>
2022-08-09 15:08:08 +0100

    Second exercice of loops, now with data and model

---
+++ exercicios/pages/render/repeticao2.jsx
@@ -0,0 +1,35 @@
+import listaProdutos from "../../data/listaProdutos"
+
+export default function repeticao2() {
+    const comBorda = {
+        border: "1px solid #fff"
+    }
+
+    function renderizarLinhas(){
+        return listaProdutos.map(produto =>{
+            return (
+                <tr key={produto.id}>
+                    <td style={comBorda}>{produto.id}</td>
+                    <td style={comBorda}>{produto.nome}</td>
+                    <td style={comBorda}>{produto.preco}</td>
+                </tr>
+            )
+        })
+    }
+    return (
+        <div>
+            <table style={comBorda}>
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderizarLinhas()}
+                </tbody>
+            </table>
+        </div>
+    )
+}
