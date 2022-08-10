75f1a1bdf0bb651743f3a0bdadf539fb93083fb7
renatotnk <renato-tnk@live.com>
2022-08-10 10:55:15 +0100

    Using React States

--- exercicios/components/indireta1/Filho.jsx
+++ exercicios/components/indireta1/Filho.jsx
@@ -2,7 +2,11 @@ export default function Filho(props) {
     return(
         <div>
             <h1>Filho </h1>
-            <button onClick={() => props.funcao("Passe no ENEM!")}>Falar com o Pai</button>
+            <button onClick={
+                () => props.funcao("Passei no ENEM!")
+            }>
+                Falar com o Pai
+            </button>
         </div>
     )
 }
---
+++ exercicios/pages/estado/mouse.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+export default function mouse() {
+
+    const [x, setX] = useState(0)
+
+    const arrayY = useState(0)
+    let y = arrayY[0]
+    const alterarY = arrayY[1]
+
+    const estilo = {
+        backgroundColor: "#222",
+            color: "#fff",
+            fontSize: "150%",
+            height: "100vh",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+    }
+
+    function quandoMover(ev){
+        setX(ev.clientX)
+        alterarY(ev.clientY)   
+    }
+
+    return(
+        <div style={estilo} onMouseMove={quandoMover}>
+            <div>Eixo X : {x}</div>
+            <div>Eixo Y : {y}</div>
+        </div>
+    )
+}
