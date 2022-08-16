f27a2d5724ac8cdfec99214acdfb2e684aa7ac61
renatotnk <renato-tnk@live.com>
2022-08-16 17:42:41 +0100

    Adding Door events and controlled Door component

--- portas/components/Porta/Porta.tsx
+++ portas/components/Porta/Porta.tsx
@@ -1,25 +1,36 @@
-import styles from "./Porta.module.css"
-import PortaModel from "../../model/Porta"
+import styles from "./Porta.module.css";
+import PortaModel from "../../model/Porta";
 
-interface PortaProps{
-    porta: PortaModel
+interface PortaProps {
+  value: PortaModel;
+  onChange: (novaPorta: PortaModel) => void;
 }
 
 export default function Porta(props: PortaProps) {
-    const {porta} = props
-    const selecionada = porta.selecionada ? styles.selecionada : ''
+  const porta = props.value;
+  const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : "";
 
-    return(
-        <div className={styles.area}>
-            <div className={`${styles.estrutura} ${selecionada}`}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>{porta.numero}</div>
-                    <div className={styles.macaneta}></div>
-                </div>
-            </div>
-            <div className={styles.chao}>
+  const alternarSelecao = (e) => props.onChange(porta.alternarSelecao());
+  const abrir = (e) => {
+    e.stopPropagation();
+    props.onChange(porta.abrir());
+  };
 
-            </div>
-        </div>
-    )
+  function renderizarPorta() {
+    return (
+      <div className={styles.porta}>
+        <div className={styles.numero}>{porta.numero}</div>
+        <div className={styles.macaneta} onClick={abrir}></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.area} onClick={alternarSelecao}>
+      <div className={`${styles.estrutura} ${selecionada}`}>
+        {porta.aberta ? false : renderizarPorta()}
+      </div>
+      <div className={styles.chao}></div>
+    </div>
+  );
 }
---
+++ portas/pages/index.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Porta from "../components/Porta/Porta";
+import PortaModel from "../model/Porta";
+
+export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
+  return (
+    <div style={{display: "flex"}}>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+    </div>
+  )
+}
