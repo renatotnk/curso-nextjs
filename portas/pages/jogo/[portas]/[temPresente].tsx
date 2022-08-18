74400dff6e1f28a481d8c25aaba7358fbb9f5d08
renatotnk <renato-tnk@live.com>
2022-08-18 14:25:49 +0100

    Form Validation

--- portas/pages/jogo/[portas]/[temPresente].tsx
+++ portas/pages/jogo/[portas]/[temPresente].tsx
@@ -9,12 +9,24 @@ export default function Jogo() {
     const router = useRouter()
 
     const [portas, setPortas] = useState([])
+    const [valido, setValido] = useState(false)
 
     useEffect(() => {
         const portas = +router.query.portas
         const temPresente = +router.query.temPresente
         setPortas(criarPortas(portas, temPresente))
     }, [router?.query])
+
+    useEffect(() => {
+        const portas = +router.query.portas
+        const temPresente = +router.query.temPresente
+
+        const qtdePortasValida = portas >= 3 && portas <= 100
+        const temPresenteValido = temPresente >= 1 &&  temPresente <= portas
+
+        setValido(qtdePortasValida && temPresenteValido)
+        
+    }, [portas])
 
     function renderizarPortas(){
         return portas.map(porta => {
@@ -29,7 +41,10 @@ export default function Jogo() {
     return(
         <div id={styles.jogo}>
             <div className={styles.portas}>
-                {renderizarPortas()}
+                {valido ? 
+                    renderizarPortas() :
+                    <h2>Valores Inválidos</h2>
+                }
             </div>
             <div className={styles.botoes}>
                 <Link href="/">
