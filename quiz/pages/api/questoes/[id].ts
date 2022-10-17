c148da30cd0adb8f9de763d0670480e00611ee04
renatotnk <renato-tnk@live.com>
2022-10-17 14:25:39 +0100

    Initializing the API...

---
+++ quiz/pages/api/questoes/[id].ts
@@ -0,0 +1,15 @@
+import questoes from "../bancodeQuestoes"
+
+export default function handler(req, res) {
+    const idSelecionado = +req.query.id
+
+    const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+
+    if (unicaQuestaoOuNada.length === 1) {
+        const questaoSelecionada = unicaQuestaoOuNada[0]
+        res.status(200).json(questaoSelecionada.paraObjeto())
+    } else {
+        res.status(204).send()
+    }
+
+}
