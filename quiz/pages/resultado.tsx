a8f112d601b40862edbf7b85b34069c28b3f1f8d
renatotnk <renato-tnk@live.com>
2022-11-11 23:40:30 +0000

    Adding results to final screen with new component

--- quiz/pages/resultado.tsx
+++ quiz/pages/resultado.tsx
@@ -1,7 +1,24 @@
+import { useRouter } from "next/router"
+import Estatistica from "../components/Estatistica/Estatistica"
+import Botao from "../components/Botao/Botao"
+import styles from '../styles/Resultado.module.css'
+
 export default function Resultado(props) {
+    const router = useRouter()
+
+    const total = +router.query.total
+    const certas = +router.query.certas
+    const percentual = Math.round((certas / total) * 100)
+
     return(
-        <div>
-            <h1>Resultado</h1>
+        <div className={styles.resultado}>
+            <h1>Resultado Final</h1>
+            <div style={{display:"flex"}}>
+                <Estatistica texto="Perguntas" valor={total}/>
+                <Estatistica texto="Certas" valor={certas} corFundo="#9cd2a4"/>
+                <Estatistica texto="Percentual" valor={`${percentual}%`} corFundo="#de6a33"/>
+            </div>
+            <Botao href="/" texto="Tentar Novamente"/>
         </div>
     )
 }
